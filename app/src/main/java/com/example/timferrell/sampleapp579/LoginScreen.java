6ad1ac3139b1a3779df03191370631e05f5ef798
Timothy <tferrell@usc.edu>
2016-02-17 12:09:54 -0800

    Decent looking version

--- app/src/main/java/com/example/timferrell/sampleapp579/LoginScreen.java
+++ app/src/main/java/com/example/timferrell/sampleapp579/LoginScreen.java
@@ -19,8 +19,6 @@ public class LoginScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
     }
 
